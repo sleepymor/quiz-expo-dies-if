e9c7d259ba7879841b0bd64ec26b868ba85e183f
ZAZW27 <clawsrage27@gmail.com>
2024-11-06 15:26:33 +0800

    added some designs

---
+++ app/Livewire/TopScore.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TopScore extends Component
+{
+    public function render()
+    {
+        return view('livewire.top-score');
+    }
+}
---
+++ resources/views/livewire/top-score.blade.php
@@ -0,0 +1,13 @@
+<section class="content text-white z-50 h-[500px]">
+    <div class="score">
+        <h3 class="animated-pop">Top Scores</h1>
+        <div>
+            <section class="top-three">
+                <div class="podium">
+                    <h1>1</h1>
+                    <p>Zharif Aziz</p>
+                </div>
+            </section>
+        </div>
+    </div>
+</section>

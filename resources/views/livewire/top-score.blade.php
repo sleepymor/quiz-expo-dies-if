2442af57fbbcd35750034bc34057a0a1811179d3
RaziMaulana <razifaturmaulana@gmail.com>
2025-08-25 08:56:17 +0800

    clear all styling

--- resources/views/livewire/top-score.blade.php
+++ resources/views/livewire/top-score.blade.php
@@ -1,22 +1,21 @@
-<section class="content text-white z-50 h-[500px]">
+<section class="content z-50 h-[500px]">
     <div class="score">
-        <h3 class="animated-pop">Top Scores</h1>
-            <div>
-
-                <section class="top-three">
-                    @foreach ($highscores as $highscore)
-                        @if ($highscore && $highscore->player)
-                            <div class="podium">
-                                <h1>{{ $loop->index + 1 }}</h1>
-                                <p>{{ $highscore->player->username }}</p>
-                            </div>
-                        @else
-                            <p>Belum ada data</p>
-                        @endif
-                    @endforeach
-                </section>
-                <section class="top-ten">
-                </section>
-            </div>
+        <h3 class="animated-pop">Top Scores</h3>
+        <div>
+            <section class="top-three">
+                @foreach ($highscores as $highscore)
+                    @if ($highscore && $highscore->player)
+                        <div class="podium">
+                            <h1>{{ $loop->index + 1 }}</h1>
+                            <p>{{ $highscore->player->username }}</p>
+                        </div>
+                    @else
+                        <p>Belum ada data</p>
+                    @endif
+                @endforeach
+            </section>
+            <section class="top-ten">
+            </section>
+        </div>
     </div>
 </section>
